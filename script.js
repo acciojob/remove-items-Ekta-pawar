daa636e2f7893d711334ac919189ef1b47390284
Ekta-pawar <ektapawar0001@gmail.com>
2025-07-27 11:26:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeColor() {
+  const section = document.querySelector("#colorSelect");
+  const selectedIndex = section.selectedIndex;
+  if (selectedIndex !== -1) {
+    section.remove(selectedIndex);
+  }
+}
